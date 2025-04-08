63310fc57e4e2a8dd4f6aa7b0b07fc693f3e285c
MuhammadAsad20 <121595889+MuhammadAsad20@users.noreply.github.com>
2025-04-08 09:32:57 -0700

    Update BestSeller.jsx

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -15,8 +15,8 @@ function BestSeller() {
                             <div className="h-full flex flex-col items-center text-center">
                                 <img alt="team" className="flex-shrink-0 rounded-full w-50 h-50 object-cover object-center mb-4" src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Zm9vZHxlbnwwfHwwfHx8MA%3D%3D" />
                                 <div className="w-full  flex flex-col items-center ">
-                                    <h2 className="title-font font-medium text-lg text-gray-900">Holden Caulfield</h2>
-                                    <p className="w-3/4 mb-4 text-sm">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                    <h2 className="title-font font-bold text-xl text-gray-900"  style={{ fontFamily: 'Great Vibes, cursive' }}>Holden Caulfield</h2>
+                                    <p className="w-3/4 mb-4 text-md">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                                 </div>
                             </div>
                         </div>
@@ -24,8 +24,8 @@ function BestSeller() {
                             <div className="h-full flex flex-col items-center text-center">
                                 <img alt="team" className="flex-shrink-0 rounded-full w-50 h-50 object-cover object-center mb-4" src="https://plus.unsplash.com/premium_photo-1675252369719-dd52bc69c3df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGZvb2R8ZW58MHx8MHx8fDA%3D" />
                                 <div className="w-full  flex flex-col items-center ">
-                                    <h2 className="title-font font-medium text-lg text-gray-900">Atticus Finch</h2>
-                                    <p className="w-3/4 mb-4 text-sm">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                    <h2 className="title-font font-bold text-xl text-gray-900" style={{ fontFamily: 'Great Vibes, cursive' }}>Atticus Finch</h2>
+                                    <p className="w-3/4 mb-4 text-md">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                                 </div>
                             </div>
                         </div>
@@ -33,8 +33,8 @@ function BestSeller() {
                             <div className="h-full flex flex-col items-center text-center">
                                 <img alt="team" className="flex-shrink-0 rounded-full w-50 h-50 object-cover object-center mb-4" src="https://images.unsplash.com/photo-1534080564583-6be75777b70a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTc1fHxmb29kfGVufDB8fDB8fHww" />
                                 <div className="w-full flex flex-col items-center ">
-                                    <h2 className="title-font font-medium text-lg text-gray-900">Jeanette Mccarthy</h2>
-                                    <p className="w-3/4 mb-4 text-sm">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
+                                    <h2 className="title-font font-bold text-xl text-gray-900" style={{ fontFamily: 'Great Vibes, cursive' }}>Jeanette Mccarthy</h2>
+                                    <p className="w-3/4 mb-4 text-md">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
                                 </div>
                             </div>
                         </div>
